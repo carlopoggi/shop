0835bbc53d8987c1476dbe42f935a12d3b6df9a7
Carlo <carlo.poggi98@gmail.com>
2022-07-18 18:06:46 +0200

    update negozio initial state

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -12,7 +12,11 @@ const Shop = () => {
   const [cart, setCart] = useState({})
   const [totale, setTotale] = useState(0)
   const negozio = {
-    '24242uu': { nome: 'pesca', prezzo: 12 }
+    '24242uu': { nome: 'pesca', prezzo: 12 },
+    '3383838': { nome: 'mela', prezzo: 1 },
+    '282737j': { nome: 'banana', prezzo: 2 },
+    '0000222': { nome: 'kiwi', prezzo: 20 },
+    '88822aa': { nome: 'pere', prezzo: 102 }
   }
   
   const getShopItem = (id) => {
