992ca5762d6804f05ac21d22d78180ece2f80a2b
Carlo <carlo.poggi98@gmail.com>
2022-07-18 17:51:54 +0200

    Add total to Shop

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,27 +1,46 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import _forEach from 'lodash/forEach'
 import _filter from 'lodash/filter'
 import _keys from 'lodash/keys'
+import _isEmpty from 'lodash/isEmpty'
 
 import List from './List'
+import Total from './Total'
 
 
 const Shop = () => {
   const [cart, setCart] = useState({})
+  const [totale, setTotale] = useState(0)
   const negozio = {
     '24242uu': { nome: 'pesca', prezzo: 12 }
   }
-
+  
   const getShopItem = (id) => {
     return negozio[id] || {}
   }
-
+  
   const getCartItem = (id) => {
     return cart[id] || {}
   }
-
+  
   const getCartQty = (id) => {
     return getCartItem(id).qty || 0
+  }
+
+  const getCartItemPrice = (id) => {
+    return getCartItem(id).prezzo || 0
+  }
+
+  const updateTotal = () => {
+    const cartKeys = _keys(cart)
+    let updatedTotal = 0
+    _forEach(cartKeys, (currentKey) => {
+      const amount = getCartQty(currentKey)
+      const price = getCartItemPrice(currentKey)
+      return updatedTotal += amount * price 
+    })
+
+    setTotale(updatedTotal)
   }
   
   const onIncrement = (id) => {
@@ -42,7 +61,9 @@ const Shop = () => {
 
     if (currentQty <= 1) {
       const keys = _filter(
-        _keys(cart), (current) => current !== id)
+        _keys(cart), (current) => (
+          current !== id)
+        )
       
       let res = {}
       _forEach(keys, (k) => {
@@ -50,6 +71,7 @@ const Shop = () => {
       })
 
       setCart(res)
+      setTotale(0)
     }
     setCart({
       ...cart,
@@ -59,6 +81,12 @@ const Shop = () => {
       } 
     })
   }
+
+  useEffect(() => {
+    if (!_isEmpty(cart)) {
+      updateTotal()
+    }
+  }, [cart])
 
   return (
     <>
@@ -74,6 +102,7 @@ const Shop = () => {
         onIncrement={onIncrement}
         onDecrement={onDecrement}
       />
+      <Total total={totale} />
     </>
   )
 }
