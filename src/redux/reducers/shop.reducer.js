9089c78dba94f48ae9a7710c9a31119bcaf78dbb
carlopoggi <carlo.poggi98@gmail.com>
2022-08-03 18:44:10 +0200

    add shop reducer

---
+++ src/redux/reducers/shop.reducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+  '24242uu': { nome: 'pesca', prezzo: 12 },
+  '3383838': { nome: 'mela', prezzo: 1 },
+  '282737j': { nome: 'banana', prezzo: 2 },
+  '0000222': { nome: 'kiwi', prezzo: 20 },
+  '88822aa': { nome: 'pere', prezzo: 102 }
+}
+
+const shop = (state = initialState, action) => {
+  switch(action.type) {
+    default: return state
+  }
+}
+
+export default shop
