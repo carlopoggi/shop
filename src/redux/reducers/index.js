46d7bd634d13df07c76d81ef569f1852ac93dae8
carlopoggi <carlo.poggi98@gmail.com>
2022-08-04 12:12:53 +0200

    add total reducer to combinereducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,9 +2,10 @@ import { combineReducers } from "redux";
 
 import cart from "./cart.reducer";
 import shop from './shop.reducer'
-
+import total from "./total.reducer";
 
 export default combineReducers({
   cart,
   shop,
+  total,
 })
